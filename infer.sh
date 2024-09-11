651a8e87e7706978bb9fcad92b68f1d2a21fc701
hyliang96 <hyliang96@hotmail.com>
2024-09-11 18:09:23 +0800

    modified pip requrements (successfully run on g32 cuda_11.2); run inference with pretrained checkpoints; preprocess LibriTTS to f0
    remove f0_vqvae/f0_vqvae.pth, because it is the same with the f0_vqvae.pth from https:
    //drive.google.com/drive/folders/1tDIQ5Nv-X2svhcww35LWMC1El3SDlI_I?
    usp=sharing, which is provided in https://github.com/hayeong0/DDDM-VC?
    tab=readme-ov-file#-pre-trained-model

--- infer.sh
+++ infer.sh
@@ -1,8 +1,8 @@
 python3 inference.py \
-    --src_path './sample/src_p227_013.wav' \
-    --trg_path './sample/tar_p229_005.wav' \
-    --ckpt_model './ckpt/G_520000.pth' \
-    --ckpt_voc './vocoder/voc_ckpt.pth' \
-    --ckpt_f0_vqvae './f0_vqvae/G_720000.pth' \
+    --src_path './sample/cn_woman.wav' \
+    --trg_path './sample/cn_man.wav' \
+    --ckpt_model './ckpt/model_base.pth' \
+    --ckpt_voc './ckpt/voc_ckpt.pth' \
+    --ckpt_f0_vqvae './ckpt/f0_vqvae.pth' \
     --output_dir './converted' \
     -t 6
--- preprocess/extract_f0.py
+++ preprocess/extract_f0.py
@@ -1,31 +1,33 @@
 import os
 import glob
 import random
-import argparse 
+import argparse
 import numpy as np
-from tqdm import tqdm 
+from tqdm import tqdm
 
 import torch
 import torchaudio
 from torch.utils.data import DataLoader
 import torch.multiprocessing as mp
-import torch.distributed as dist 
+import torch.distributed as dist
 
 import torchaudio.functional as F
-import librosa 
+import torchaudio.transforms as T
+import librosa
 import amfm_decompy.basic_tools as basic
 import amfm_decompy.pYAAPT as pYAAPT
 
 def get_yaapt_f0(audio, sr=16000, interp=False):
+    # sr: sample rate
     to_pad = int(20.0 / 1000 * sr) // 2
     f0s = []
     for y in audio.astype(np.float64):
-        y_pad = np.pad(y.squeeze(), (to_pad, to_pad), "constant", constant_values=0) 
-        pitch = pYAAPT.yaapt(basic.SignalObj(y_pad, sr), 
+        y_pad = np.pad(y.squeeze(), (to_pad, to_pad), "constant", constant_values=0)
+        pitch = pYAAPT.yaapt(basic.SignalObj(y_pad, sr),
                              **{'frame_length': 20.0, 'frame_space': 5.0, 'nccf_thresh1': 0.25, 'tda_frame_length': 25.0})
         f0s.append(pitch.samp_interp[None, None, :] if interp else pitch.samp_values[None, None, :])
 
-    return np.vstack(f0s)   
+    return np.vstack(f0s)
 
 
 def main(args):
@@ -48,7 +50,7 @@ def run(rank, n_gpus, args):
         dset,
         num_replicas=n_gpus,
         rank=rank,
-        shuffle=True)
+        shuffle=False)
 
     collate_fn = Collate()
     d_loader = DataLoader(dset, num_workers=8, shuffle=False,
@@ -56,20 +58,20 @@ def run(rank, n_gpus, args):
                           drop_last=False, collate_fn=collate_fn, sampler=d_sampler)
 
     for epoch in range(1):
-        extract(d_loader, rank)
+        extract(d_loader, args.output_dir, rank)
 
 
 class DLoader():
     def __init__(self, input_dir):
         self.wavs = []
-        self.wavs += sorted(glob.glob(os.path.join(input_dir, '**/*.wav'), recursive=True))  
+        self.wavs += sorted(glob.glob(os.path.join(input_dir, '**/*.wav'), recursive=True))
         print('wav num: ', len(self.wavs))
 
     def __getitem__(self, index):
         return self.wavs[index]
 
     def __len__(self):
-        return len(self.wavs)  
+        return len(self.wavs)
 
 class Collate():
     def __init__(self):
@@ -78,30 +80,36 @@ class Collate():
     def __call__(self, batch):
         return batch[0]
 
-def extract(d_loader, rank):
+def extract(d_loader, output_dir, rank, resample_rate=16000):
     with torch.no_grad():
-        for _, audio_path in enumerate(tqdm(d_loader)):    
-            f0_filename = audio_path.replace("LibriTTS_16k", "LibriTTS_f0").replace(".wav", ".pt")
-            
+        pbar = tqdm(d_loader)
+        for _, audio_path in enumerate(pbar):
+            pbar.set_description(os.path.basename(audio_path))
+            f0_filename = os.path.join(output_dir, os.path.basename(audio_path).replace(".wav", ".pt"))
+
             if not os.path.isfile(f0_filename):
-                os.makedirs(os.path.dirname(f0_filename), exist_ok=True)  
-                
-                audio, sr = torchaudio.load(audio_path)
+                os.makedirs(os.path.dirname(f0_filename), exist_ok=True)
+
+                audio, sample_rate = torchaudio.load(audio_path)
+                resampler = T.Resample(sample_rate, resample_rate, dtype=audio.dtype)
+                audio = resampler(audio)
+
                 p = (audio.shape[-1] // 1280 + 1) * 1280 - audio.shape[-1]
                 audio = torch.nn.functional.pad(audio, (0, p), mode='constant').data
 
                 try:
-                    f0 = get_yaapt_f0(audio.numpy())
+                    f0 = get_yaapt_f0(audio.numpy(), sr=resample_rate)
                 except:
                     f0 = np.zeros((1, 1, audio.shape[-1] // 80))
-    
-                f0 = torch.FloatTensor(f0.astype(np.float32).squeeze(0)) 
+
+                f0 = torch.FloatTensor(f0.astype(np.float32).squeeze(0))
                 torch.save(f0, f0_filename)
 
 
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_dir', default='/workspace/raid/dataset/LibriTTS_16k') 
-    a = parser.parse_args() 
+    parser.add_argument('-i', '--input_dir', default='/data/haoyu/data/LibriTTS/extract')
+    parser.add_argument('-o', '--output_dir', default='/data/haoyu/data/LibriTTS/LibriTTS_16k_f0')
+    a = parser.parse_args()
     main(a)
