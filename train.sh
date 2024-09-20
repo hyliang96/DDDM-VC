34746b50baed7639fb7d2f2a59f1d93587ae3002
hyliang96 <hyliang96@hotmail.com>
2024-09-21 01:47:48 +0800

    train try0

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
-config_path="./configs/libritts.json"
-model_name="libritts"
+config_path="./configs/config.json"
+model_name="try0"
 
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 export HF_HUB_OFFLINE=1
-python train_dddmvc.py -c $config_path -m $model_name
+python train_dddmvc.py  -c $config_path -m $model_name
