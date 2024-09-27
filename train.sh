d2d407ee41b730282701342d387076dba51a61ea
hyliang96 <hyliang96@hotmail.com>
2024-09-27 10:48:47 +0800

    bs=64

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 config_path="./configs/config.json"
-model_name="try0.1"
+model_name="bs64"
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export HF_HUB_OFFLINE=1
 python train_dddmvc.py -c $config_path -m $model_name
